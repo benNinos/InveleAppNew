e484d3e6bb2b0a276bdf47a96afc83dd13c4666b
pbsarathy21 <pbsarathy21@gmail.com>
2019-03-20 10:25:00 +0530

    Commited by partha

---
+++ app/src/main/java/com/ninositsolution/inveleapp/pojo/HomeArrayLists.java
@@ -0,0 +1,45 @@
+package com.ninositsolution.inveleapp.pojo;
+
+import java.util.ArrayList;
+
+class HomeArrayLists {
+
+    public Integer menu_id;
+    public String name;
+    public String image_path;
+    public String slug;
+    public ArrayList<HomeArrayLists> child_menus;
+    public Integer id;
+    public Integer home_management_id;
+    public String image;
+    public String banner_image;
+    public String url;
+    public String color;
+    public String starting_date;
+    public String ending_date;
+    public String position;
+    public String seller_id;
+    public String sku;
+    public Integer status;
+    public Integer approde_status;
+    public Integer min_price;
+    public Integer max_price;
+    public String created_at;
+    public String updated_at;
+    public Integer product_id;
+    public String usual_price;
+    public Integer invele_price;
+    public String discount;
+    public Integer average_rating;
+    public Integer wishlist;
+    public String category_name;
+    public String theme_type;
+    public String theme_color;
+    public String caption;
+    public String advertisement;
+    public String banner_type;
+    public String start_date_time;
+    public String end_date_time;
+    public ArrayList<HomeArrayLists> home_management_products;
+    public ArrayList<HomeArrayLists> home_management_images;
+}
--- app/src/main/java/com/ninositsolution/inveleapp/pojo/POJOClass.java
+++ app/src/main/java/com/ninositsolution/inveleapp/pojo/POJOClass.java
@@ -1,5 +1,6 @@
 package com.ninositsolution.inveleapp.pojo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class POJOClass {
@@ -15,5 +16,14 @@ public class POJOClass {
     public List<AddressList>address_list;
     public List<AddressList>addresses;
     public AddressList user_address;
+    public ArrayList<HomeArrayLists> menus;
+    public ArrayList<HomeArrayLists> main_banners;
+    public ArrayList<HomeArrayLists> sub_banners;
+    public ArrayList<HomeArrayLists> deal_products;
+    public ArrayList<HomeArrayLists> home_managements;
+    public ArrayList<HomeArrayLists> product_trendings;
+    public ArrayList<HomeArrayLists> brands;
+    public String start_date_time;
+    public String end_date_time;
 
 }
